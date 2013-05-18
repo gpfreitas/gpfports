2fcc60e113fba3457634c25aeefc5b3080670579
guilherme.freitas@openx.com <guilherme.freitas>
2013-05-18 00:09:59 +0000

    More changes including PREFIX

--- bashreduce/install_bashreduce.sh
+++ bashreduce/install_bashreduce.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 
-cd ~/src
+cd "$PREFIX"/src
 git clone https://github.com/erikfrey/bashreduce.git
 
-cd ~/src/bashreduce
-sed -i .bak 's:/usr/local/bin:~/bin:g' brutils/Makefile
+cd "$PREFIX"/src/bashreduce
+sed -i .bak "s:/usr/local/bin:$PREFIX/bin:g" brutils/Makefile
 
-cd ~/src/bashreduce/brutils
+cd "$PREFIX"/src/bashreduce/brutils
 make
 make install
 
--- pv/install_pv.sh
+++ pv/install_pv.sh
@@ -3,15 +3,15 @@
 PREFIX="$HOME/local"
 
 test -d "$PREFIX"/src || mkdir "$PREFIX"/src
-test -d "$PREFIX"/src/pv || mkdir "$PREFIX"/src/pv
 
-cd "$PREFIX"/src/pv \
+cd "$PREFIX"/src \
     && curl -O http://www.ivarch.com/programs/sources/pv-1.4.6.tar.gz \
     && curl -O http://www.ivarch.com/programs/sources/pv-1.4.6.tar.gz.txt \
     && gpg --verify pv-1.4.6.tar.gz.txt pv-1.4.6.tar.gz \
     && gunzip pv-1.4.6.tar.gz && tar -xf pv-1.4.6.tar \
     && cd pv-1.4.6 \
     && ./configure --prefix="$PREFIX" \
-    && make
+    && make \
+    && make install
 
 
